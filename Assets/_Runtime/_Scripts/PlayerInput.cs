7613e23daf822672bff877bedf417f4c5dacea0a
Xicovon <37887143+Xicovon@users.noreply.github.com>
2021-09-14 17:44:10 -0400

    Add files via upload

---
+++ Assets/_Runtime/_Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            Vector3 direction = new Vector3(horizontal, 0, vertical);
+            direction = direction.normalized;
+
+            transform.position += direction * Time.deltaTime * 5;
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction, Vector3.up), Time.deltaTime * 300);
+        }
+    }
+}
