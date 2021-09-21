f993a26268c1d16cb11c0aca9fc02cb5d8299a91
Xicovon <37887143+Xicovon@users.noreply.github.com>
2021-09-21 17:28:16 -0400

    lab2

---
+++ lab2/New Unity Project/Assets/_Runtime/_Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private bool isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Time.timescale = 0;
+                panel.SetActive(true);
+                isPaused = true;
+            }
+            else
+            {
+                Time.timescale = 1;
+                panel.SetActive(true);
+                isPaused = true;
+            }
+            
+        }
+    }
+}
---
+++ lab2/New Unity Project/Assets/_Runtime/_Scripts/ReturnToMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    public bool pause = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
